c3044519bfe0732512ff225a2fd2f89bf304cdb1
Saad Ahmad <ahmadsaad58@gmail.com>
2019-05-15 23:51:09 -0500

    Created skeleton code

--- snake.py
+++ snake.py
@@ -5,6 +5,48 @@ import tkinter as tk
 from tkinter import messagebox
 
 
+class cube(object): 
+	row = 0
+	column = 0
+
+	def __init__(self, start, dirnx = 1, dirny = 0, color = (255, 0, 0)):
+		pass
+	
+	def move(self, dirnx, dirny):
+		pass
+	
+	def draw(self, surface, eyes = False): 
+		pass
+
+class snake(object):
+
+	def __init__(self, color, pos):
+		pass
+	
+	def move(self):
+		pass
+	
+	def reset(self):
+		pass
+	
+	def addCube(self):
+		pass
+
+	def draw(self):
+		pass
+	
+def drawGrid(column, row, surface):
+	pass
+
+def redrawWindow(surface):
+	pass
+
+def randomSnack(row, items):
+	pass
+
+def message_box(subject, content):
+	pass
 
 def main(): 
-	window = pygame.display
+	#window = pygame.display
+	pass
